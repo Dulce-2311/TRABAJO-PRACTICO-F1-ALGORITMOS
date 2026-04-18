dd00ca657ca5be9a6c2ee3a37ca4f8c5ae196300
Dulce Correa <dulce.correa@estudiante.ucsm.edu.pe>
2026-04-17 23:28:38 -0500

    Se crea la clase GestorZonas con inicialización de matriz bidimensional

--- src/proyecto/modelo/GestorZonas.java
+++ src/proyecto/modelo/GestorZonas.java
@@ -1,5 +1,14 @@
 package proyecto.modelo;
 
 public class GestorZonas {
+    private Paquete<T>[][] matriz;
+    private int zonas;
+    private int paquetesPorZona;
 
+    public GestorZonas(int zonas, int paquetesPorZona) {
+        this.zonas = zonas;
+        this.paquetesPorZona = paquetesPorZona;
+        matriz = new Paquete[zonas][paquetesPorZona];
+    }
+    
 }
