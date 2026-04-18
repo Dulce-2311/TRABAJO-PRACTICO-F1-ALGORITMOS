4ac9baa7742e341448cf0644686fd0c6954335eb
Dulce Correa <dulce.correa@estudiante.ucsm.edu.pe>
2026-04-17 23:20:30 -0500

    Se inicializa la lista de paquetes en el constructor

--- src/proyecto/lectura/LectorArchivo.java
+++ src/proyecto/lectura/LectorArchivo.java
@@ -1,5 +1,17 @@
 package proyecto.lectura;
 
+import proyecto.modelo.Paquete;
+import java.util.ArrayList;
+
 public class LectorArchivo {
+    private int capacidad;
+    private int zonas;
+    private int paquetesPorZona;
+    
+ private ArrayList<Paquete<String>> listaPaquetes;
+
+    public LectorArchivo() {
+        listaPaquetes = new ArrayList<>();
+    }
 
 }
