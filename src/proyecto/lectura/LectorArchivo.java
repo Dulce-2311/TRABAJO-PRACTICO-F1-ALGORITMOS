b74236bf252173086f6164bda00a31a044755f37
Dulce Correa <dulce.correa@estudiante.ucsm.edu.pe>
2026-04-17 23:21:49 -0500

    Se implementa la estructura base para lectura de archivos

--- src/proyecto/lectura/LectorArchivo.java
+++ src/proyecto/lectura/LectorArchivo.java
@@ -1,17 +1,32 @@
 package proyecto.lectura;
 
 import proyecto.modelo.Paquete;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class LectorArchivo {
     private int capacidad;
     private int zonas;
     private int paquetesPorZona;
-    
+
  private ArrayList<Paquete<String>> listaPaquetes;
 
     public LectorArchivo() {
         listaPaquetes = new ArrayList<>();
     }
+
+    public void leerArchivo(String ruta) {
+        try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
+
+            String linea;
+
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        }
+    }    
+
 
 }
